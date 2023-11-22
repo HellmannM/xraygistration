9a07c1ea9c81ede6d6cf61af90cdd57ab4887f26
Matthias Hellmann <m.hellmann@me.com>
2023-11-22 19:22:07 +0100

    fix argc

--- src/dnnmatch/main.py
+++ src/dnnmatch/main.py
@@ -13,7 +13,10 @@ renderer_wrapper = renderlib.create_renderer
 renderer_wrapper.restype = ctypes.c_void_p
 renderer = ctypes.c_void_p(renderer_wrapper())
 
-arg_buffers = [ctypes.create_string_buffer(256) for i in range(2)]
-arg_buffers = ["./src/match/match\0", "../testfiles/Dummy_Paul_nifti/2__head_10_stx_head.nii\n"]
-arg_ptrs    = (ctypes.c_char_p*2)(*map(ctypes.addressof, arg_buffers))
-renderlib.init_renderer(renderer, 2, arg_ptrs)
+arg_buffers = [ctypes.create_string_buffer(b"./src/match/match"),
+               ctypes.create_string_buffer(b"../testfiles/Dummy_Paul_nifti/2__head_10_stx_head.nii"),
+               ctypes.create_string_buffer(b"-device"),
+               ctypes.create_string_buffer(b"gpu")
+              ]
+arg_ptrs    = (ctypes.c_char_p*4)(*map(ctypes.addressof, arg_buffers))
+renderlib.init_renderer(renderer, 4, arg_ptrs)
