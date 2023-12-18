31b9fa32581c324ca2c355faacc568b863ed2846
Matthias Hellmann <m.hellmann@me.com>
2023-12-18 18:38:59 +0100

    fix runtime errors

--- src/dnnmatch/resnet.py
+++ src/dnnmatch/resnet.py
@@ -1,5 +1,6 @@
 # resnet.py
 
+import sys
 import matplotlib.pyplot as plotter_lib
 import numpy as np
 import PIL as image_lib
@@ -11,7 +12,7 @@ from tensorflow.keras.optimizers import Adam
 
 class DataGenerator(tf.keras.utils.Sequence):
     'Generates data for Keras'
-    def __init__(self, dim=(32,32,32), batch_size=128, batches_per_epoch=128, n_channels=1):
+    def __init__(self, dim=(32,32), batch_size=128, batches_per_epoch=128, n_channels=3):
         'Initialization'
         self.dim = dim
         self.batch_size = batch_size
@@ -30,53 +31,53 @@ class DataGenerator(tf.keras.utils.Sequence):
     def __data_generation(self):
         'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
         # Initialization
-        #X = np.empty((self.batch_size, *self.dim, self.n_channels))
-        X = np.empty((self.batch_size, self.n_channels, *self.dim))
+        X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        #X = np.empty((self.batch_size, self.n_channels, *self.dim))
         y = np.empty((self.batch_size, 9), dtype=np.float32)
 
         # Generate data
         for i in range(self.batch_size):
-            y = (0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
-            #TODO X[i,] = get_frame(y)
-            X[i,0,] = [[i, 1], [2, 3]]
+            y[i] = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+            #TODO X[i,] = get_frame(y[i])
+            X[i,] = np.zeros((self.dim[0], self.dim[1], self.n_channels))
+        
+        X = tf.keras.applications.resnet_v2.preprocess_input(X, data_format='channels_last')
 
         return X, y
 
 
-# import ResNet-50 model from keras library
-# Setting include_top to False means it will allow adding input and output layers custom to a problem.
-# The weights parameter specifies that the model uses its weights while training on the imagenet dataset.
+# Parameters
+#TODO get dims
+dim_x = 32
+dim_y = 32
+params = {'dim': (dim_x, dim_y),
+          'batch_size': 8,
+          'n_channels': 3}  #n_channels must be 3 for resnet
+
+# Setup model
 model = Sequential()
-pretrained_model_for_demo= tf.keras.applications.ResNet50(include_top=False,
-                   input_shape=(180,180,3),
+# add pretrained resnet model and make layers non-trainable
+pretrained_model = tf.keras.applications.ResNet50V2(include_top=False,
+                   input_shape=(dim_x, dim_y, 3),
                    pooling='avg',
                    weights='imagenet')
-for each_layer in pretrained_model_for_demo.layers:
+for each_layer in pretrained_model.layers:
         each_layer.trainable=False
-model.add(pretrained_model_for_demo)
-
+model.add(pretrained_model)
 # add a fully connected output layer
 model.add(Flatten())
 model.add(Dense(512, activation='relu'))
 # 3 vec3: eye, dir, up
 model.add(Dense(9, activation='linear'))
-
-
-#keras.losses.MeanSquaredError(reduction="sum_over_batch_size", name="mean_squared_error")
-model.compile(optimizer=Adam(learning_rate=0.001), loss='mean_squared_error', metrics=['accuracy'])
-
-# Parameters
-#TODO get dims
-params = {'dim': (2,2,2),
-          'batch_size': 128,
-          'n_channels': 1}
+model.compile(optimizer=Adam(learning_rate=0.001), loss='mean_squared_error', metrics=['mean_squared_error'])
+model.summary()
 
 # Generators
-training_generator   = DataGenerator(batches_per_epoch=1024, **params)
-validation_generator = DataGenerator(batches_per_epoch=128, **params)
+training_generator   = DataGenerator(batches_per_epoch=128, **params)
+validation_generator = DataGenerator(batches_per_epoch=32, **params)
 
 # Train model on dataset
-epochs=10
+epochs=8
 history = model.fit(x=training_generator,
                     validation_data=validation_generator,
                     epochs=epochs,
@@ -85,10 +86,10 @@ history = model.fit(x=training_generator,
 
 # evaluate
 plotter_lib.figure(figsize=(8, 8))
-epochs_range= range(epochs)
-plotter_lib.plot( epochs_range, history.history['accuracy'], label="Training Accuracy")
-plotter_lib.plot(epochs_range, history.history['val_accuracy'], label="Validation Accuracy")
-plotter_lib.axis(ymin=0.4,ymax=1)
+epochs_range = range(epochs)
+plotter_lib.plot( epochs_range, history.history['mean_squared_error'], label="Training Accuracy")
+plotter_lib.plot(epochs_range, history.history['val_mean_squared_error'], label="Validation Accuracy")
+plotter_lib.axis(ymin=0, ymax=np.max(history.history['val_mean_squared_error']))
 plotter_lib.grid()
 plotter_lib.title('Model Accuracy')
 plotter_lib.ylabel('Accuracy')
