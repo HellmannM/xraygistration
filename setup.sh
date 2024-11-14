7eef4b07a36e6de796595dd3350366e658652ff7
Matthias Hellmann <m.hellmann@me.com>
2024-11-14 14:59:16 +0100

    remove obsolete deskvox code

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd "${SCRIPT_DIR}"
 
 BUILD_VSNRAY=1
-BUILD_DESKVOX=0
 BUILD_ITK=1
 
 NUM_CORES=24
@@ -24,24 +23,6 @@ if [ $BUILD_VSNRAY == "1" ]; then
         -DVSNRAY_ENABLE_VIEWER=OFF \
         -DVSNRAY_ENABLE_PEDANTIC=OFF \
         -DVSNRAY_ENABLE_PTEX=OFF
-    make clean
-    make -j$NUM_CORES
-    popd 
-fi
-
-# 3rdparty/deskvox
-if [ $BUILD_DESKVOX == "1" ]; then
-    VISIONARAY_DIR="$PWD/3rdparty/visionaray"
-    pushd 3rdparty/deskvox
-    mkdir build
-    cd build
-    cmake .. \
-        -DCMAKE_POLICY_DEFAULT_CMP0072=NEW \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_CXX_FLAGS="-march=native -I$VISIONARAY_DIR/build/config" \
-        -DCUDA_NVCC_FLAGS="-I$VISIONARAY_DIR/build/config" \
-        -DVISIONARAY_INCLUDE_DIR="$VISIONARAY_DIR/include" \
-        -DVISIONARAY_LIBRARY="$VISIONARAY_DIR/build/src/visionaray/libvisionaray.so"
     make clean
     make -j$NUM_CORES
     popd 
